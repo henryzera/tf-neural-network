7874dc8b8880546633ea695d565502e9c95a910b
Emanuel Henry <emanuelhenrysc@gmail.com>
2026-09-16 20:55:24 -0300

    feat: training model

--- index.js
+++ index.js
@@ -1,5 +1,71 @@
 import tf from '@tensorflow/tfjs-node';
 
+async function trainModel(inputXs, outputYs){
+    const model = tf.sequential()
+
+    // Primeira camada da rede:
+    // entrada de 7 posições (idade normalizada + 3 cores + 3 localizações)
+
+    // 80 neuronios = aqui coloquei tudo isso, pq tem pouca base de treino
+    // quanto mais neuronios, mais complexidade a rede pode aprender
+    // e consequentemente, mais processamento ela vai usar
+
+    // A ReLU age como um filtro:
+    // É como se ela deixasse somente os dados interessantes seguirem viagem na rede 
+    // Se a informação chegou nesse neuronio é positiva, passa para frente!
+    // se for zero ou negativa, pode jogar fora, nao vai servir para nada
+    model.add(tf.layers.dense({ inputShape: [7], units: 80, activation: 'relu' }))
+
+    // Saída: 3 neuronios
+    // um para cada categoria (premium, medium, basic)
+
+    // activation: softmax normaliza a saída em probabilidades
+    model.add(tf.layers.dense({units: 3, activation: 'softmax'}))
+
+    // Compilando o modelo
+    // optimizer Adam ( Adaptive Moment Estimation )
+    // é um treinador pessoal moderno para redes neurais:
+    // ajusta os pesos de forma eficiente e inteligente
+    // aprender com histórico de erros e acertos
+
+    // loss: categoricalCrossentropy
+    // Ele compara o que o modelo "acha" (os scores de cada categoria)
+    // com a resposta certa
+    // a categoria premium será sempre [1, 0, 0]
+
+    // quanto mais distante da previsão do modelo da resposta correta
+    // maior o erro (loss)
+    // Exemplo classico: classificação de imagens, recomendação, categorização de
+    // usuário
+    // qualquer coisa em que a resposta certa é "apenas uma entre várias possíveis"
+    model.compile({
+        optimizer: 'adam',
+        loss: 'categoricalCrossentropy',
+        metrics: ['accuracy']
+    })
+
+    // Treinamento do modelo
+    // verbose: desabilita o log interno (e usa só callback)
+    // epochs: quantidade de vezes que vai rodar no dataset
+    // shuffle: embaralha os dados para evitar viés
+    await model.fit(
+        inputXs,
+        outputYs,
+        {
+            verbose: 0,
+            epochs: 100,
+            shuffle: true,
+            callbacks: {
+                onEpochEnd: (epoch, log) => console.log(
+                    `Epoch: ${epoch}: loss = ${log.loss}`
+                )
+            }
+        }
+    )
+
+    return model
+}
+
 // Exemplo de pessoas para treino (cada pessoa com idade, cor e localização)
 // const pessoas = [
 //     { nome: "Erick", idade: 30, cor: "azul", localizacao: "São Paulo" },
@@ -36,5 +102,7 @@ const tensorLabels = [
 const inputXs = tf.tensor2d(tensorPessoasNormalizado)
 const outputYs = tf.tensor2d(tensorLabels)
 
-inputXs.print();
-outputYs.print();
+// Quanto mais dado melhor!
+// assim o algoritimo consegue entender melhor os padrões complexos
+// dos dados
+const model = trainModel(inputXs, outputYs)
